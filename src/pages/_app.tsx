93e6b6c7509c70aeafa3122f7875c6c73606fa7f
netogerbi <netogerbi2@hotmail.com>
2023-04-23 19:31:28 -0300

    configured built in nextjs abs path

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import GlobalStyles from '~/styles/global';
+import GlobalStyles from 'styles/global';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
