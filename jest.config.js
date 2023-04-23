070e412586011ad20f34c3f1d12b4adda9dd1fad
netogerbi <netogerbi2@hotmail.com>
2023-04-23 18:43:39 -0300

    styled components and eslint missing configuration and integration

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
+  collectCoverageFrom: ['src/**/*.tsx', 'src/**/*.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  testMatch: [
+    '<rootDir>/src/**/*test.tsx',
+    '<rootDir>/src/**/*test.ts',
+  ]
 };
