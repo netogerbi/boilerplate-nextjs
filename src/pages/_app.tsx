9a9f417099fcaf3fd4552aa87e1929b394aaadef
netogerbi <netogerbi2@hotmail.com>
2023-04-23 19:26:09 -0300

    add global styles in global app of nextjs

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import GlobalStyles from '~/styles/global';
+
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon512" />
+        <link rel="apple-touch-icon" href="/img/icon512" />
+        <meta name="description" content="boilerplate" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
+}
